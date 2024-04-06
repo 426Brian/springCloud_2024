bb58812ca5ced9320132cd8e924aaccb95963a5e
Brian <bryan426@qq.com>
2024-04-06 14:48:25 +0800

    circuitBreak slidewindow
    Signed-off-by: Brian <bryan426@qq.com>

--- cloud2024/cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud2024/cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -19,10 +19,10 @@ public class FeignConfig {
 
     @Bean
     Retryer retryer(){
-//        return Retryer.NEVER_RETRY; // Feign 默认不走重试策略
+        return Retryer.NEVER_RETRY; // Feign 默认不走重试策略
 
 //         最大请求次数为3，出事间隔时间为100ms, 重试最大间隔时间为1s.
-        return new Retryer.Default(100, 1, 3);
+//        return new Retryer.Default(100, 1, 3);
 
     }
 
